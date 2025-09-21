d1a2733d7c4f99c09c797fe4a9fa7706f5935eed
Toorpu Vinod Kumar Reddy <136839828+vinodreddytoorpu@users.noreply.github.com>
2025-09-21 20:54:58 +0530

    added data memory

--- src/verif/env/amba_apb5_requester_subscriber.sv
+++ src/verif/env/amba_apb5_requester_subscriber.sv
@@ -8,6 +8,8 @@ class amba_apb5_requester_subscriber extends uvm_subscriber #(apb5_packet #(`AMB
 
   amba_environment env;
 
+  bit [`AMBA_APB5_DATA_WIDTH-1:0] data_mem [bit [`AMBA_APB5_ADDR_WIDTH-1:0]];
+
   extern function new(string name = "amba_apb5_requester_subscriber", uvm_component parent);
   extern function void build_phase(uvm_phase phase);
   extern function void write(apb5_packet #(`AMBA_APB5_ADDR_WIDTH, `AMBA_APB5_DATA_WIDTH, `AMBA_APB5_USER_REQ_WIDTH, `AMBA_APB5_USER_DATA_WIDTH) t);
@@ -27,7 +29,11 @@ function void amba_apb5_requester_subscriber::build_phase(uvm_phase phase);
 endfunction : build_phase
 
 function void amba_apb5_requester_subscriber::write(apb5_packet #(`AMBA_APB5_ADDR_WIDTH, `AMBA_APB5_DATA_WIDTH, `AMBA_APB5_USER_REQ_WIDTH, `AMBA_APB5_USER_DATA_WIDTH) t);
-  `uvm_info(get_name(), $psprintf("[amba_apb5_requester_subscriber] received transaction from apb5 requester monitor to subscriber: \n%0s", t.sprint()), UVM_LOW)
+  `uvm_info(get_name(), $psprintf("[amba_apb5_requester_subscriber] received transaction from apb5 requester monitor to subscriber: \n%0s", t.sprint()), UVM_HIGH)
+  if(t.write == 1'b0) begin
+    data_mem[t.addr] = t.wdata;
+    `uvm_info(get_name(), $psprintf("Write Transaction : storing into data_mem[%0h] = %0h", t.addr, t.wdata), UVM_LOW)
+  end
 endfunction : write
     
 task amba_apb5_requester_subscriber::main_phase(uvm_phase phase);
