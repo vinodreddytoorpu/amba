74618279ebed8e321e45a81aaafe19c012fd2d71
Toorpu Vinod Kumar Reddy <136839828+vinodreddytoorpu@users.noreply.github.com>
2025-09-21 20:55:27 +0530

    write followed by read transfer

--- src/verif/test/seqs/amba_apb5_requester_write_read_seq.sv
+++ src/verif/test/seqs/amba_apb5_requester_write_read_seq.sv
@@ -18,8 +18,22 @@ task amba_apb5_requester_write_read_seq::body();
     write        == 0;
     strb         == {`AMBA_APB5_DATA_WIDTH/8{1'b1}};
     wdata        == 'hDEADBEEF;
-    wakeup       == 1'b1;
+    wakeup       == 1'b0;
     b2b_transfer == 1'b0;
   }) `uvm_fatal("SEQ", "Randomization failed for apb5_requester_seq")
   apb5_seq.start(env.v_seqr.apb5_req_seqr[0]);
+
+  #100ns;
+
+  apb5_seq = apb5_requester_base_seq #(`AMBA_APB5_ADDR_WIDTH, `AMBA_APB5_DATA_WIDTH, `AMBA_APB5_USER_REQ_WIDTH, `AMBA_APB5_USER_DATA_WIDTH)::type_id::create("apb5_seq");
+  if(!apb5_seq.randomize() with { 
+    addr inside {[`AMBA_APB5_COMPLETER0_ADDR_START : `AMBA_APB5_COMPLETER0_ADDR_END]};
+    write        == 1;
+    strb         == {`AMBA_APB5_DATA_WIDTH/8{1'b1}};
+    wdata        == 'hDEADBEEF;
+    wakeup       == 1'b0;
+    b2b_transfer == 1'b0;
+  }) `uvm_fatal("SEQ", "Randomization failed for apb5_requester_seq")
+  apb5_seq.start(env.v_seqr.apb5_req_seqr[0]);
+
 endtask : body
